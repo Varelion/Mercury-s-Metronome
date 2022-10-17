26b2ea19c21c8aaa8f1f4cc26131f37edfc1145c
EightFiveZeroOne <business.8508@gmail.com>
2022-10-17 09:09:08 -0400

    Updating the project

---
+++ content.js
@@ -0,0 +1,19 @@
+// Here we define the things related to the extension
+// can't deploy an extension without this file.
+
+// Select server based on ID
+// Select channel based on ID
+// Above can be ignored if you just leave the thing open
+// Run timer,
+// Once timer = 0 send first message
+// Enter
+// send second message
+// Enter
+// Start timer again
+// Repeat
+
+// https://discord.com/channels/serverID/channelID
+    // Discord Server: 907088886411431976
+    // Server's Channel : 907451784656736346
+
+    
